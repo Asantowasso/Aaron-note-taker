bf615e6aca3b7e6c1245e2b0fdf9e05fef22ccc1
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-10-29 17:14:43 -0400

    rearranged files and added server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+// A page to a link with the appllication
+// The left side will display the existing notes
+// When I enter a new title with text a save icon is shown at the top of the page
+// The new note is saved and appears in the left hand column with other notes. When I click on this note in appears in the right hand column.
+// Clicking on the write icon gives the user an empty field to type in 
+
+const express = require('express')
+const api = require ('./routes/index.js')
+
+const PORT = process.env.port || 3001;
+
+const app = express();
