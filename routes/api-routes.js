a4026c2ee3ff51a761309381d51354cc137e5a41
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-10-31 21:56:52 -0400

    added ids to notes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,7 @@
 const { randomUUID } = require("crypto")
 // const notes = require("../db/db.json")
 const fs = require ("fs")
-
+console.log(typeof randomUUID)
 module.exports = function (app){
 app.get("/api/notes", function (req, res){
     const notes = JSON.parse(fs.readFileSync("./db/db.json"))
@@ -10,7 +10,7 @@ app.get("/api/notes", function (req, res){
 app.post("/api/notes", (req, res) => {
     const notes = JSON.parse(fs.readFileSync("./db/db.json"))
     const newNotes = req.body;
-    newNotes.id = randomUUID
+    newNotes.id = randomUUID()
     notes.push(newNotes);
     fs.writeFileSync("./db/db.json", JSON.stringify(notes))
     res.json(notes);
