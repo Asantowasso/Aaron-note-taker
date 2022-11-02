ee56ee39703fe9e02d00312a740588f99109c311
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-11-01 20:52:34 -0400

    added README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Aaron-note-taker
+
+## Description
+
+The note taker is designed to allow the user to write and save notes be these for daily tasks, important dates or times and other need to know information.
+
+## Usage
+
+Persons needing to plan a schedule, create reminders or write down vital information. Notes are entered by clicking the plus symbol on the top right of the page. Then below this a title and body text can be entered. These notes will appear on the left side of the screen. When clicked these notes will display their contents on the right hand side of the page.
+
+## Screenshot
+<img src = "./assets/Aaron notes taker.jpg">
+
+
+## Deployed Application
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// A page to a link with the appllication
+// A page to a link with the application
 // The left side will display the existing notes
 // When I enter a new title with text a save icon is shown at the top of the page
 // The new note is saved and appears in the left hand column with other notes. When I click on this note in appears in the right hand column.
@@ -8,7 +8,6 @@ const express = require('express')
 
 const fs = require('fs')
 
-// const api = require ('./index.js')
 
 const path = require('path')
 
@@ -20,7 +19,6 @@ const app = express();
 
 
 
-// globalThis.windowVar =
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname +'/public')))
