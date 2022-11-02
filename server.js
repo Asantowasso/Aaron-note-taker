5fa7c4a0af4f29ba1ddc641128032fbe8b124283
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-11-02 18:27:58 -0400

    Updating status

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// A page to a link with the application
+// A page to a link with the appllication
 // The left side will display the existing notes
 // When I enter a new title with text a save icon is shown at the top of the page
 // The new note is saved and appears in the left hand column with other notes. When I click on this note in appears in the right hand column.
@@ -8,6 +8,7 @@ const express = require('express')
 
 const fs = require('fs')
 
+// const api = require ('./index.js')
 
 const path = require('path')
 
@@ -19,6 +20,7 @@ const app = express();
 
 
 
+// globalThis.windowVar =
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname +'/public')))
